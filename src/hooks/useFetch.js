162bd413a6c2cb75a79fa8f2fc120f2510501997
Amit Chaudhari <amitkumarchaudhari151@gmail.com>
2025-07-31 17:36:47 +0530

    useFetch.js

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(url);
+        if (!res.ok) throw new Error('Failed to fetch');
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, [url]);
+
+  return { data, loading, error };
+};
