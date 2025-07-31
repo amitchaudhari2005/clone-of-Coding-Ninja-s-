1dfcec2772aaa0006fc4e4ef44eb0edd2099e0df
Amit Chaudhari <amitkumarchaudhari151@gmail.com>
2025-07-31 17:02:04 +0530

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import HomePage from './pages/Home';
+import AboutPage from './pages/About';
+import DashboardPage from './pages/Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
