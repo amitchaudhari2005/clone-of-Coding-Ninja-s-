99f55fddd0b200b528d6164f2cac29dfa83eb7db
Amit Chaudhari <amitkumarchaudhari151@gmail.com>
2025-07-31 17:32:46 +0530

    constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+// Format date to DD/MM/YYYY
+export const formatDate = (dateStr) => {
+  const date = new Date(dateStr);
+  return date.toLocaleDateString('en-IN');
+};
+
+// Capitalize first letter
+export const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
